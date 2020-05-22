fcedaee0b3ae5960caa834741a04793d916a114f
Eric Suh <yoonjjsuh@gmail.com>
2020-05-21 21:25:05 -0400

    add toggle and remove functionality

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,22 +1,45 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { toggleTodo, removeTodo } from '../../redux/todosSlice';
 
 import './TodoItem.scss';
 
+const mapDispatch = { toggleTodo, removeTodo };
+
 const TodoItem = (props) => {
-  const { text } = props;
+  const {
+    text, toggleTodo, removeTodo, id, completed
+  } = props;
+
+  const handleToggle = () => {
+    toggleTodo(id);
+  };
+
+  const handleDelete = () => {
+    removeTodo(id);
+  };
+
+  const todoLabelClass = classNames('todo-item__label', {
+    'todo-item__label--completed': completed
+  });
+
   return (
-    <div className="todo-item">
-      <input className="todo-item__toggle" type="checkbox" />
-      <li className="todo-item__label">{text}</li>
-      <button className="todo-item__delete" type="button">X</button>
-    </div>
+    <li className="todo-item">
+      <input className="todo-item__toggle" type="checkbox" onClick={handleToggle} />
+      <input className={todoLabelClass} value={text} />
+      <button className="todo-item__delete" type="button" onClick={handleDelete}>X</button>
+    </li>
   );
 };
 
 TodoItem.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired
 };
 
-export default TodoItem;
+export default connect(null, mapDispatch)(TodoItem);
--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -7,9 +7,17 @@ const todosSlice = createSlice({
   reducers: {
     addTodo: (state, action) => {
       state.push({ id: uuidv4(), text: action.payload, completed: false });
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((item) => item.id === action.payload);
+      todo.completed = !todo.completed;
+    },
+    removeTodo: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload);
+      state.splice(index, 1);
     }
   }
 });
 
 export default todosSlice.reducer;
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, toggleTodo, removeTodo } = todosSlice.actions;
